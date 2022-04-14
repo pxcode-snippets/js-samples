241b2354b1c3af31456b47757764a57c2479aadd
pxCode <54593724+px2code@users.noreply.github.com>
2022-04-15 03:55:26 +0800

    Update section-fullscreen.js

--- section-fullscreen.js
+++ section-fullscreen.js
@@ -1,14 +1,19 @@
 
-function init() {
+function init(mq) {  
+  const className = 'h-100vh';
   const nodes = document.querySelectorAll('.h100');
-  nodes.forEach( e => e.style.height = '100vh' );
-  console.log(`init called ${nodes.length} applied`);
+  nodes.forEach( e => {    
+    mq.matches ? e.classList.remove(className) : e.classList.add(className);
+    // e.style.height = '100vh';
+  });
+  console.log(`init called ${nodes.length} applied at mq ${mq.matches}`);
 }
 
-
+const mq = window.matchMedia("(max-width: 768px)")
 document.addEventListener("DOMContentLoaded", function(){
   console.log('Loaded');
-  init();
+  init(mq);
 });
 
-init();
+mq.addListener(init);
+init(mq);
